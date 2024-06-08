9eed72d96ceb835de629eb43211933d137fbf035
AlanCarp91218 <alandominguezcarp1@gmail.com>
2024-06-08 20:15:24 -0300

    cambio de header con logo incluido (a ajustar)

--- model/PreguntasModel.php
+++ model/PreguntasModel.php
@@ -11,7 +11,7 @@ class PreguntasModel
     public function getPreguntaRandom($userId){
         // Traigo una pregunta que el usuario aún no ha respondido
         $stmt = $this->database->prepare("
-        SELECT * FROM PREGUNTAS 
+        SELECT * FROM preguntas 
         WHERE _id NOT IN (
             SELECT PREGUNTA_ID FROM PREGUNTAS_JUGADAS WHERE USER_ID = ?
         )
@@ -26,17 +26,17 @@ class PreguntasModel
 
     public function addPreguntaJugada($userId, $preguntaId, $esCorrecta)
     {
-        $stmt = $this->database->prepare("INSERT INTO PREGUNTAS_JUGADAS (USER_ID, PREGUNTA_ID, ES_CORRECTA) VALUES (?, ?, ?)");
+        $stmt = $this->database->prepare("INSERT INTO preguntas_jugadas (USER_ID, PREGUNTA_ID, ES_CORRECTA) VALUES (?, ?, ?)");
         $this->database->execute($stmt, ["iii", $userId, $preguntaId, $esCorrecta ? 1 : 0]);
     }
 
     public function getRespuestas($preguntaId){
-        $stmt = $this->database->prepare("SELECT * FROM RESPUESTAS WHERE ID_PREGUNTA = ?");
+        $stmt = $this->database->prepare("SELECT * FROM respuestas WHERE ID_PREGUNTA = ?");
         return $this->database->execute($stmt, ["i", $preguntaId]);
     }
 
     public function comprobarRespuesta($respuestaId){
-        $stmt = $this->database->prepare("SELECT es_correcta FROM RESPUESTAS WHERE _ID = ?");
+        $stmt = $this->database->prepare("SELECT es_correcta FROM respuestas WHERE _ID = ?");
         $result = $this->database->execute($stmt, ["i", $respuestaId]);
 
         // Si la consulta devuelve al menos un resultado y es_correcta es 1, entonces la respuesta es correcta
